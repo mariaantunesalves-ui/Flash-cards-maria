0d63fd856d016307a2f7af6eb5c8b02fe3f9aec2
mariaantunesalves-ui <maria.antunes.alves@escola.pr.gov.br>
2025-09-09 08:46:18 -0300

    09/09

---
+++ Perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Biologia',
+    'Qual o animal mais rápido do mundo?',
+    'Guepardo'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual o único alimento quenunca estraga?',
+    'Mel'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual a cor mais rara da natureza?',
+    'Azul'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o maior oceano do planeta?',
+    'Oceano Pácifico'
+)
+
+criaCartao(
+    'Curiosidades',
+    'Qual o artefato mais caro que já foi vendido?',
+    'Obra de arte “Salvator Mundi” do artista Leonardo da Vinci'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o maior deserto do mundo?',
+    'Deserto da Antártica'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual o animal que emite o som mais alto do mundo?',
+    'Baleia Azul'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o país que tem mais línguas faladas no mundo?',
+    'Papua-Nova Guiné'
+)
+
+criaCartao(
+    'Curiosidades',
+    'Qual a estátua mais pesada do mundo?',
+    'Estátua da liberdade'
+)
