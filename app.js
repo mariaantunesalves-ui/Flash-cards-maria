c5bfe64c8709a0a4d22ba1debed4020b8f9dc992
mariaantunesalves-ui <maria.antunes.alves@escola.pr.gov.br>
2025-09-16 10:25:11 -0300

    16/09

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ function criaCartao(categoria, pergunta, resposta) {
 
     cartao.innerHTML = `
     <div class="cartao__conteudo">
-    <h3>CATEGORIA</h3>
+    <h3>${cartegoria}</h3>
     <div class="cartao__conteudo__pergunta">
-    <p> PERGUNTA - escrever a pergunta</p>
+    <p>${pergunta}</p>
     </div>
     <div class="cartao__conteudo__resposta">
-    <p> RESPOSTA - escrever a resposta</p>
+    <p>${resposta}</p>
     </div>
     </div>`
 }
