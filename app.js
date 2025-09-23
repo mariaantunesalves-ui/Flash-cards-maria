c3550584108e849f6d5299a34a7a2b90035637b7
mariaantunesalves-ui <maria.antunes.alves@escola.pr.gov.br>
2025-09-23 09:27:13 -0300

    23/09

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ function criaCartao(categoria, pergunta, resposta) {
 let respostaEstaVisivel = false
 
 function viraCartao( ){
-    respostaEstaVisivel = !respostaEstaVisivel
+    respostaEstaVisivel=!respostaEstaVisivel
 }
 
     container.appendChild(cartao)
